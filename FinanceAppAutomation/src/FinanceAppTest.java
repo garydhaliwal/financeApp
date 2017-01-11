972d2d961245225c407b8a4f67a524c9390ead52
wtw-exercise <gurpreet.dhaliwal@gmail.com>
2017-01-11 16:34:38 -0500

    Update FinanceAppTest.java

--- FinanceAppAutomation/src/FinanceAppTest.java
+++ FinanceAppAutomation/src/FinanceAppTest.java
@@ -33,8 +33,8 @@ public class FinanceAppTest extends UiAutomatorTestCase {
 		
 		//Output percentage comparisons to console
 		Log.d("Output", "Todays price of $" + wtwStockData[0] +
-				" is " + (wtwStockData[0]/wtwStockData[2] * 10) + "% lower than the 52 week high " +
-						"and is " + (wtwStockData[0]/wtwStockData[1] * 10) + "% higher than the 52 week low");
+				" is " + (wtwStockData[0]/wtwStockData[2] * 100) + "% lower than the 52 week high " +
+						"and is " + (wtwStockData[0]/wtwStockData[1] * 100) + "% higher than the 52 week low");
 
 		
 		//Retrieve WDAY stock data
